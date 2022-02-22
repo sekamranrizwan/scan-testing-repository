483bb78684bb787b3f54a1c2d01f35bb76050ea9
sekamranrizwan <sekamranrizwan@gmail.com>
2022-02-22 17:20:56 +0500

    added error

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 function App() {
+  const username = "<script><img onerror='alert(\"Hacked!\")' src='invalid-image' /></script>";
   return (
     <div className='App'>
-      <input type='search' value='Attacker ' /> <script> StealCredentials() </script>{' '}
+      <input type='search' value='Attacker ' /> {username}
     </div>
   );
 }
