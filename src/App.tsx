73491569596de1e76ddd44502ef93a24e159f8e3
sekamranrizwan <sekamranrizwan@gmail.com>
2022-02-22 11:47:40 +0500

    codeql initialize

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,5 @@
 import React from 'react';
-
 function App() {
-  return (
-    <div className='App'>
-      <svg
-        className='text-black pb-6'
-        width='58'
-        viewBox='0 0 99 57'
-        xmlns='http://www.w3.org/2000/svg'
-        preserveAspectRatio='xMidYMin meet'
-      >
-        <path
-          fill='currentCOlor'
-          d='M0 52.432L46.813 0v33.241L0 52.431zm80.516-20.705L99 52.432l-46.826-19.19V0H70.51c11.438 0 19.16 7.517 19.16 16.773.014 6.534-3.714 12.192-9.155 14.954zM2.972 57l46.521-19.07L96.014 57H2.972z'
-        />
-      </svg>
-    </div>
-  );
+  return <div className='App'>abc</div>;
 }
-
 export default App;
