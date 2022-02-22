c5b8e2724d7e3c19c3c6debddc11e9d048de8970
sekamranrizwan <sekamranrizwan@gmail.com>
2022-02-22 12:31:49 +0500

    removed permission thing

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,12 +1,9 @@
 import { ReportHandler } from 'web-vitals';
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import('web-vitals').then(({ getCLS, getFID }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
     });
   }
 };
