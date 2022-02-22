75019f1819a4cdc05f60ef0c09e7648677c8a50d
sekamranrizwan <sekamranrizwan@gmail.com>
2022-02-22 17:36:32 +0500

    added error

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,15 @@ function App() {
     document.write('<OPTION value=1>' + deflt + '</OPTION>');
     document.write('<OPTION value=2>English</OPTION>');
   }
+
+  const customPropsControledByAttacker = {
+    dangerouslySetInnerHTML: {
+      __html: "<img onerror='alert(\"Hacked!\");' src='invalid-image' />",
+    },
+  };
   return (
     <div className='App'>
-      <input type='search' value='Attacker ' /> <script> {setLanguageOptions()} </script>{' '}
+      <input type='search' value='Attacker ' /> <script> {customPropsControledByAttacker} </script>{' '}
     </div>
   );
 }
